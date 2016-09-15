4a8fc7194e0070c2292bbaa0484b44fca70cabdf
Damian Lattenero <damianlattenero@gmail.com>
2016-09-15 01:17:36 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+
+gem "compass"
+# gem "rails"
+ruby '2.3.1'
---
+++ app/scripts/config.js
@@ -0,0 +1,7 @@
+'use strict';
+
+ angular.module('config', [])
+
+.constant('ENV', {name:'production',apiEndpoint:'http://subi-que-te-llevo-api.herokuapp.com/rest/'})
+
+;
