1c1a858dbab1dc35adf021d017097937a9d3a47b
Cristian Marchionne <cdmarchionne@gmail.com>
2016-10-12 22:35:16 -0300

    Correcciones de Angular

--- app/controllers/foodOrder/new.js
+++ app/controllers/foodOrder/new.js
@@ -9,41 +9,32 @@
 
 angular.module('myApp')
   .controller('NewFoodOrderCtrl', function ($scope, FoodOrderService, $window) {
-    $scope.newFoodOrder = {
-      productId: 0,
-      productAmount: 0
-    };
+      this.newFoodOrder = {
+        productId: 0,
+        productAmount: 0
+      };
 
-    $scope.setId = function (id) {
-      $scope.newFoodOrder.productId = id;
+      this.orderProduct = function (foodOrder) {
+        foodOrder.productAmount = 1;
+        this.save(foodOrder);
+      };
+
+      this.cookProduct = function (foodOrder) {
+        foodOrder.productAmount = (-1);
+        this.save(foodOrder);
+      };
+
+      this.save = function (foodOrder) {
+        FoodOrderService.save(foodOrder)
+          .then(function successCallback(response) {
+            this.updatePending(foodOrder.productId, response.data.productPending);
+          });
+      };
+
+      this.updatePending = function (productId, pending) {
+        var product = document.getElementById(productId);
+        product.getElementsByClassName("productPending")[0].innerHTML = pending;
+      };
+
     }
-
-    $scope.orderProduct = function (foodOrder) {
-      foodOrder.productAmount = 1;
-      // $scope.addAnimation(foodOrder,"orderProduct");
-      $scope.save(foodOrder);
-    };
-
-    $scope.cookProduct = function (foodOrder) {
-      foodOrder.productAmount = (-1);
-      //$scope.addAnimation(foodOrder,"cookProduct");
-      $scope.save(foodOrder);
-    };
-
-    $scope.save = function (foodOrder) {
-      FoodOrderService.save(foodOrder)
-        .then(function successCallback(response) {
-            $scope.updatePending(foodOrder.productId, response.data.productPending);
-        });
-    };
-    $scope.updatePending = function (productId, pending) {
-      var product = document.getElementById(productId);
-      product.getElementsByClassName("productPending")[0].innerHTML = pending;
-    };
-
-    $scope.addAnimation = function (foodOrder, buttonClass) {
-      var product = document.getElementById(foodOrder.productId);
-      var button = product.getElementsByClassName("FoodOrderActions")[0].getElementsByClassName(buttonClass)[0];
-      button.addClass('animated bounceOutLeft');
-    };
-  });
+  );
