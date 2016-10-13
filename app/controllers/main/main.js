88e9d221abff4339600fc48b993613b1376c7a17
Cristian Marchionne <cdmarchionne@gmail.com>
2016-10-12 22:30:17 -0300

    Refactorizando Angular

--- app/controllers/main/main.js
+++ app/controllers/main/main.js
@@ -8,10 +8,25 @@
  */
 
 angular.module('myApp')
-  .controller('MainCtrl', function ($scope, FoodOrderService, ProductService, $window) {
+  .controller('MainCtrl', function ($scope, NewFoodOrderCtrl, ProductService, $window) {
 
-    ProductService.getAll()
-      .then(function successCallback(response) {
-        $scope.allProducts = response.data;
-      });
+    var self = this;
+
+    this.getProducts = function () {
+      ProductService.getAll()
+        .then(function successCallback(response) {
+          self.products = response.data;
+        });
+    }
+
+    this.getProducts();
+
+    this.orderProduct = function (productId) {
+      var foodOrder = NewFoodOrderCtrl.newFoodOrder;
+      foodOrder.productId = productId;
+      foodOrder.orderProduct(foodOrder);
+      var product = self.products.filter(function (p) { return p.productId == productId; });
+      product[0].pending += 1;
+    };
+
   });
