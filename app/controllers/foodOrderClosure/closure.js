908289543a3bbcb39f15eb8f262fc4e49f788724
cris marchionne <cdmarchionne@gmail.com>
2016-11-17 03:17:50 -0300

    revisar cambios en la vista del cierre

--- app/controllers/foodOrderClosure/closure.js
+++ app/controllers/foodOrderClosure/closure.js
@@ -9,25 +9,35 @@
  */
 
 angular.module('myApp')
-  .controller('ClosureCtrl', function($scope, FoodOrderClosureService, authService, $routeParams) {
+  .controller('ClosureCtrl', function ($scope, FoodOrderClosureService, authService, $routeParams) {
     var self = this;
 
-    $scope.loading = true;
+    // $scope.loading = true;
 
     this.generateClosureData = {
-      momentClosure: 0,
-      user: ""
+      user: "",
+      from: 0,
+      to: 0
     };
 
+    this.clousures = [];
     //vm.authService.userProfile.nickname
 
-    this.generateClosure = function (momentClosure) {
-      self.generateClosureData.momentClosure = momentClosure;
+    this.generateClosure = function (clousure) {
       self.generateClosureData.user = authService.userProfile.nickname;
+     /* self.generateClosureData.from = Date.now();
+      self.generateClosureData.to = Date.now();*/
+      self.generateClosureData.from = clousure.from.milliseconds;
+      self.generateClosureData.to = clousure.to.milliseconds;
+
+
+      console.log("generateClosureData:" + self.generateClosureData);
 
       FoodOrderClosureService.generateClosure(self.generateClosureData)
         .then(function successCallback(response) {
-          window.location.reload();
+          self.clousures = response.data;
+          console.log("closures:" + response.data);
+          // window.location.reload();
           // self.updatePending(foodOrder.productId, response.data.productPending);
         });
     };
