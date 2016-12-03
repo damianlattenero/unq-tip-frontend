d5141df64d9a14545c371f1a2a4186077fa563a6
damianlattenero <damianlattenero@gmail.com>
2016-12-03 01:15:44 -0300

    Vista de food order history y sus filtros

--- app/controllers/foodOrderHistory/all.js
+++ app/controllers/foodOrderHistory/all.js
@@ -9,22 +9,68 @@
  */
 
 angular.module('myApp')
-  .controller('FoodOrderHistoryCtrl', function (FoodOrderHistoryService) {
-    return new FoodOrderHistoryController(FoodOrderHistoryService);
+  .controller('FoodOrderHistoryCtrl', function ($scope, FoodOrderHistoryService) {
+    return new FoodOrderHistoryController($scope, FoodOrderHistoryService);
   });
 
-function FoodOrderHistoryController(FoodOrderHistoryService) {
+function FoodOrderHistoryController($scope, FoodOrderHistoryService) {
   var self = this;
 
-  this.ordersHistory = [];
+  this.orderHistories = [];
+
+  this.f = {};
+
+  this.filter_by_ge = function (field, date) {
+    console.log(field + ": " + date + " - " + new Date(date).toString('dd-MM-yyyy'));
+    if (date === '') {
+      delete self.f['ge_' + field];
+      return;
+    }
+    self.f['ge_' + field] = true;
+    self.orderHistories.forEach(function (ordersHistory) {
+      ordersHistory['ge_' + field] = (ordersHistory[field] >= date);
+    })
+  };
+
+  this.filter_by_le = function (field, date) {
+    console.log(field + ": " + date + " - " + new Date(date).toString('dd-MM-yyyy'));
+    if (date === '') {
+      delete self.f['le_' + field];
+      return;
+    }
+    self.f['le_' + field] = true;
+    self.orderHistories.forEach(function (ordersHistory) {
+      // console.log("ordersHistory: " + ordersHistory[field] + " - " + new Date(ordersHistory[field]).toString('dd-MM-yyyy'));
+      ordersHistory['le_' + field] = (ordersHistory[field] <= date);
+    })
+  };
 
   this.getAll = function () {
     FoodOrderHistoryService.getAll()
       .then(function successCallback(response) {
-        self.ordersHistory = response.data;
+        self.orderHistories = response.data;
       });
   };
 
   this.getAll();
+
+  $(function () {
+    $('#from').datetimepicker();
+    $('#to').datetimepicker({useCurrent: false});
+
+    $("#from").on("dp.change", function (e) {
+      $('#to').data("DateTimePicker").minDate(e.date);
+      var date = new Date(e.date);
+      date.setUTCHours(0,0,0,0);
+      self.filter_by_ge('moment', date.getTime())
+    });
+
+    $("#to").on("dp.change", function (e) {
+      $('#from').data("DateTimePicker").maxDate(e.date);
+      var date = new Date(e.date);
+      date.setUTCHours(0,0,0,0);
+      self.filter_by_le('moment', date.getTime())
+    });
+  });
 
 }
