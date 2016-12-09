3b6f2c7ab4cf140d8e1fb30cad734c968aaa6638
Damian Lattenero <damianlattenero@gmail.com>
2016-12-09 03:03:26 -0300

    Falta manejar la exposicion de las cache correctamente, haces muchos pedidos al backend

--- app/controllers/cache/cache.js
+++ app/controllers/cache/cache.js
@@ -9,37 +9,60 @@
  */
 
 angular.module('myApp')
-  .controller('CacheController', function (CacheService) {
-    return new CacheController(CacheService);
+  .controller('CacheController', function ($rootScope, $interval,CacheService,LoginService) {
+    return new CacheController($rootScope, $interval,CacheService,LoginService);
   });
 
-function CacheController(CacheService) {
+function CacheController($rootScope, $interval,CacheService,LoginService) {
   var self = this;
 
-  this.cacheUsers = [];
+  this.productsByUsers = [];
   this.cachePlaces = [];
   this.cacheProducts = [];
 
   this.getCacheUsers = function () {
     CacheService.getUsers()
       .then(function successCallback(response) {
-        self.orders = response.data;
+        self.productsByUsers = response.data[LoginService.getToken()].allProductsPending;
       });
   };
+
+  this.getCacheUsers();
 
   this.getCachePlaces = function () {
     CacheService.getPlaces()
       .then(function successCallback(response) {
-        self.orders = response.data;
+        self.cachePlaces = response.data;
       });
   };
+
+  this.getCachePlaces();
 
   this.getCacheProducts = function () {
     CacheService.getProducts()
       .then(function successCallback(response) {
-        self.orders = response.data;
+        self.cacheProducts = response.data;
       });
   };
+
+  this.getCacheProducts();
+
+  this.getPendingForProduct = function(productID, isFront){
+    if(isFront){
+      if (!self.productsByUsers.hasOwnProperty(productID)) {
+        return 0;
+      } else {
+        return self.productsByUsers[productID];
+      }
+    }else{
+      return self.cacheProducts[productID]
+    }
+  }
+
+  $interval(function () {
+    if ($rootScope.isAuthenticated && self.autoRefresh)
+      self.getCacheProducts();
+  }, 1000);
 
   this.getCacheUsers();
   this.getCachePlaces();
