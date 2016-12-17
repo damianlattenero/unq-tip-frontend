3041fd1d8d95e9814285966988fe37cb84a59b91
cris marchionne <cdmarchionne@gmail.com>
2016-12-17 14:15:13 -0300

    Filter History by parameters

--- app/controllers/foodOrderHistory/all.js
+++ app/controllers/foodOrderHistory/all.js
@@ -9,16 +9,45 @@
  */
 
 angular.module('myApp')
-  .controller('FoodOrderHistoryCtrl', function ($scope, FoodOrderHistoryService) {
-    return new FoodOrderHistoryController($scope, FoodOrderHistoryService);
+  .controller('FoodOrderHistoryCtrl', function ($routeParams, $timeout, FoodOrderHistoryService) {
+    return new FoodOrderHistoryController($routeParams, $timeout, FoodOrderHistoryService);
   });
 
-function FoodOrderHistoryController($scope, FoodOrderHistoryService) {
+function FoodOrderHistoryController($routeParams, $timeout, FoodOrderHistoryService) {
   var self = this;
 
   this.orderHistories = [];
 
   this.filter = {};
+
+  $(function () {
+    var dayFrom = new Date();
+    var dayTo = new Date();
+
+    var date = $routeParams.date;
+    if (!( (date == null) || (date == 0) || (date === '') )) {
+      console.log("Filtrando el historial del día: " + date);
+      dayFrom = new Date(parseInt(date));
+      dayTo = new Date(parseInt(date));
+    }
+
+    dayFrom = dayFrom.setHours(0, 0, 0, 0);
+    dayTo = dayTo.setHours(23, 59, 59, 99);
+
+    console.log("DayFrom: " + dayFrom);
+    console.log("DayTo: " + dayTo);
+
+    $('#from').datetimepicker({format: 'DD/MM/YYYY', defaultDate: dayFrom});
+    $('#to').datetimepicker({format: 'DD/MM/YYYY', defaultDate: dayTo, useCurrent: false});
+
+    $("#from").on("dp.change", function (e) {
+      $('#to').data("DateTimePicker").minDate(e.date);
+    });
+
+    $("#to").on("dp.change", function (e) {
+      $('#from').data("DateTimePicker").maxDate(e.date);
+    });
+  });
 
   this.find = function () {
     self.filter.productName = self.filterProductName;
@@ -26,7 +55,7 @@ function FoodOrderHistoryController($scope, FoodOrderHistoryService) {
     var dateFrom = $('#from').data("DateTimePicker").date();
     if (dateFrom != null) {
       dateFrom = new Date(dateFrom._d);
-      dateFrom.setUTCHours(0, 0, 0, 0);
+      dateFrom.setHours(0, 0, 0, 0);
       self.filter_by_ge(self.filter, 'moment', dateFrom.getTime());
     }
     else
@@ -36,14 +65,13 @@ function FoodOrderHistoryController($scope, FoodOrderHistoryService) {
     var dateTo = $('#to').data("DateTimePicker").date();
     if (dateTo != null) {
       dateTo = new Date(dateTo._d);
-      dateTo.setUTCHours(0, 0, 0, 0);
+      dateTo.setHours(23, 59, 59, 99);
       self.filter_by_le(self.filter, 'moment', dateTo.getTime());
     }
     else
       self.filter_by_le(self.filter, 'moment', 0);
 
   };
-
 
   this.filter_by_ge = function (filter, field, date) {
     console.log(field + ": " + date + " - " + new Date(date).toString('dd-MM-yyyy'));
@@ -79,20 +107,7 @@ function FoodOrderHistoryController($scope, FoodOrderHistoryService) {
 
   this.getAll();
 
-  $(function () {
-    var dayFrom = new Date().setHours(0,0,0,0);
-    var dayTo = new Date().setHours(23,59,59,99);
-
-    $('#from').datetimepicker({format: 'DD/MM/YYYY', defaultDate: dayFrom});
-    $('#to').datetimepicker({format: 'DD/MM/YYYY', defaultDate: dayTo, useCurrent: false});
-
-    $("#from").on("dp.change", function (e) {
-      $('#to').data("DateTimePicker").minDate(e.date);
-    });
-
-    $("#to").on("dp.change", function (e) {
-      $('#from').data("DateTimePicker").maxDate(e.date);
-    });
-  });
-
+  $timeout(function () {
+    self.find();
+  }, 80);
 }
