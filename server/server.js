1987db4670b2fdcd64269b9d9a5b6c1a524990d3
damianlattenero <damianlattenero@gmail.com>
2017-01-27 22:20:26 -0300

    cambios en las rutas

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const express = require('express')
 module.exports = {
   app: function() {
     const app = express()
-    const indexPath = path.join(__dirname, '../dist/index.html')
-    app.use(express.static(path.join(__dirname, '../dist')));
-    app.use(express.static(path.join(__dirname, '../bower_components')));
+    const indexPath = path.join(__dirname, '/dist/index.html')
+    app.use(express.static(path.join(__dirname, '/dist')));
+    app.use(express.static(path.join(__dirname, '/bower_components')));
 
     app.get('/', function(_, res) {
       res.sendFile(indexPath)
